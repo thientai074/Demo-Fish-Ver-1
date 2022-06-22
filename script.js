4aae8eb0b7184190f6643d82e48bc18b9f1df918
HaVietHung <haviethung0801@gmail.com>
2022-06-22 08:22:38 +0700

    Hoan Thanh

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let canvasPosition = canvas.getBoundingClientRect();
 
 let is_mouse_in_shape = function (x, y, enemy) {
   let shape_left = enemy.x - enemy.radius * 1.75;
-  let shape_right = shape_left + enemy.radius * 1.75;
+  let shape_right = shape_left + enemy.radius * 2.5;
   let shape_top = enemy.y - enemy.radius * 1.75;
   let shape_bottom = enemy.y + enemy.radius * 1.75;
 
@@ -110,12 +110,18 @@ class Chest {
     this.x = canvas.width * 0.5;
     this.y = canvas.height * 0.7;
     this.distance;
-    this.radius = 90;
+    this.radius = 30;
   }
   draw() {
-    ctx.fillStyle = "transparent";
+    ctx.fillStyle = "red";
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.arc(
+      canvas.width * 0.52,
+      canvas.height * 0.82,
+      this.radius,
+      0,
+      Math.PI * 2
+    );
     ctx.fill();
     ctx.closePath();
     ctx.drawImage(
@@ -129,86 +135,6 @@ class Chest {
 }
 
 const chest = new Chest();
-
-// Player
-const playerLeft = new Image();
-playerLeft.src = "../src/fish/__cartoon_fish_06_yellow_swim.png";
-const playerRight = new Image();
-playerRight.src = "../src/fish/__cartoon_fish_06_yellow_swim.png";
-
-class Player {
-  constructor() {
-    this.x = canvas.width / 2;
-    this.y = canvas.height / 2;
-    this.radius = 50;
-    this.angle = 0;
-    this.frameX = 0;
-    this.frameY = 0;
-    this.frame = 0;
-    this.spriteWidth = 498;
-    this.spriteHeight = 327;
-  }
-  update() {
-    const dx = this.x - mouse.x;
-    const dy = this.y - mouse.y;
-    let theta = Math.atan2(dy, dx);
-    this.angle = theta;
-    if (mouse.x != this.x) {
-      this.x -= dx / 20;
-    }
-    if (mouse.y != this.y) {
-      this.y -= dy / 20;
-    }
-  }
-
-  draw() {
-    if (mouse.click) {
-      ctx.lineWidth = 0.2;
-      ctx.beginPath();
-      ctx.moveTo(this.x, this.y);
-      ctx.lineTo(mouse.x, mouse.y);
-      ctx.stroke();
-    }
-    ctx.fillStyle = "transparent";
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.fill();
-    ctx.closePath();
-    ctx.fillRect(this.x, this.y, this.radius, 10);
-    ctx.save();
-    ctx.translate(this.x, this.y);
-    ctx.rotate(this.angle);
-
-    if (this.x >= mouse.x) {
-      ctx.drawImage(
-        playerLeft,
-        this.frameX * this.spriteWidth,
-        this.frameY * this.spriteHeight,
-        this.spriteWidth,
-        this.spriteHeight,
-        0 - 60,
-        0 - 45,
-        this.spriteWidth / 4,
-        this.spriteHeight / 4
-      );
-    } else {
-      ctx.drawImage(
-        playerRight,
-        this.frameX * this.spriteWidth,
-        this.frameY * this.spriteHeight,
-        this.spriteWidth,
-        this.spriteHeight,
-        0 - 60,
-        0 - 45,
-        this.spriteWidth / 4,
-        this.spriteHeight / 4
-      );
-    }
-    ctx.restore();
-  }
-}
-
-const player = new Player();
 
 // Bubbles
 
@@ -228,9 +154,6 @@ class Bubble {
   }
   update() {
     this.y -= this.speed;
-    const dx = this.x - player.x;
-    const dy = this.y - player.y;
-    this.distance = Math.sqrt(dx * dx + dy * dy);
   }
   draw() {
     ctx.fillStyle = "transparent";
@@ -256,20 +179,6 @@ function handleBubbles() {
   for (let i = 0; i < bubblesArray.length; i++) {
     bubblesArray[i].update();
     bubblesArray[i].draw();
-    if (bubblesArray[i].y < 0 - bubblesArray[i].radius * 2) {
-      bubblesArray.splice(i, 1);
-      i--;
-    } else if (
-      bubblesArray[i].distance <
-      bubblesArray[i].radius + player.radius
-    ) {
-      if (!bubblesArray[i].counted) {
-        score++;
-        bubblesArray[i].counted = true;
-        bubblesArray.splice(i, 1);
-        i--;
-      }
-    }
   }
 }
 
@@ -284,7 +193,7 @@ function randomInteger(min, max) {
 class Enemy {
   constructor() {
     this.x = canvas.width + 200;
-    this.y = randomInteger(1, 100);
+    this.y = randomInteger(1, canvas.height * 0.45);
     this.radius = 60;
     this.speed = Math.random() * 0.25 + 1;
     this.frame = 0;
@@ -299,6 +208,8 @@ class Enemy {
     ctx.fillStyle = "transparent";
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.font = "30px red Arial";
+    ctx.fillText("Hello World", this.x - 30, this.y + 30);
     ctx.fill();
     ctx.drawImage(
       enemyImage,
@@ -311,7 +222,6 @@ class Enemy {
       this.spriteWidth / 3,
       this.spriteWidth / 3
     );
-    // resizeCanvas()
   }
 
   update() {
@@ -319,7 +229,6 @@ class Enemy {
 
     if (this.x < 0 - this.radius * 2) {
       this.x = canvas.width + 200;
-      // this.y = Math.random() * (canvas.height - 150) + 90;
       this.y = randomInteger(90, 220);
       this.speed = Math.random() * 2 + 2;
     }
@@ -340,7 +249,6 @@ class Enemy {
     const dy = this.y - chest.y;
     this.distance = Math.sqrt(dx * dx + dy * dy);
     if (this.distance < this.radius + chest.radius) {
-      handleGameOver();
     }
   }
 }
@@ -356,40 +264,34 @@ function handleCatchFish() {
     if (enemiesArray[i].distance < enemiesArray[i].radius + chest.radius) {
       enemiesArray.splice(i, 1);
       is_dragging = false;
+      handleGameOver(i);
     }
   }
 }
 
 let enemiesArray = [];
 
-function handleGameOver() {
-  ctx.fillStyle = "white";
-  // ctx.fillText("Game Over, you reach score " + score, 130, 250);
+function handleGameOver(i) {
+  ctx.fillStyle = "black";
+  OpenBootstrapPopup();
+
+  ctx.fillText(`Bắt được con số ${i}`, 100, 100);
   gameOver = true;
+}
+
+function OpenBootstrapPopup() {
+  $("#myModal").modal("show");
 }
 
 // Animation Loop
 function animate() {
-  // resizeCanvas();
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   handleBubbles();
   chest.draw();
-  // player.update();
-  // player.draw();
-  // handleEnemies();
   handleCatchFish();
-  ctx.fillStyle = "black";
-  // ctx.fillText("score: " + score, 10, 50);
   gameFrame++;
   // if (!gameOver)
   requestAnimationFrame(animate);
 }
 
 animate();
-
-// canvas.addEventListener("resize", resizeCanvas());
-
-// function resizeCanvas() {
-//   canvas.width = window.innerWidth;
-//   canvas.height = window.innerHeight;
-// }
